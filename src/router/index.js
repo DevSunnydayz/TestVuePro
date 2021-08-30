e908fa393ab5c703073622210e0c0a79480be15c
Kongphop Duangthup <kongphopduangthup@MacBook-Air-khxng-Kongphop.local>
2021-08-30 16:36:30 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import testTwo from'../views/testTwo.vue'
+import testThree from'../views/testThree.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,14 +12,30 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/testOne',
+    name: 'testOne',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/testOne.vue')
+  },
+  {
+    path:'/testTwo',
+    name:'testTwo',
+    component: () => import(/* webpackChunkName: "about" */ '../views/testTwo.vue')
+  },
+  {
+    path: '/testThree',
+    name: 'testThree',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/testThree.vue')
   }
+  
+
 ]
+
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/views/testOne.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="test-one">
+    <div class="name">
+    <div class="headerfnamelname">
+      <h1>
+        Your FirstName and LastName
+      </h1>
+   <div class="input-group inSide">
+   <span class="input-group-text inputText" >First and last name</span>
+   <input type="text" aria-label="First name" class="form-control "v-model="textName.firstname">
+   <input type="text" aria-label="Last name" class="form-control" v-model="textName.lastname" >
+     </div>
+        <div class="card showName"id="uname">
+          <p>Your Name :
+           {{textName.firstname}} {{textName.lastname}}</p>      
+        </div>
+
+    </div>
+  </div>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        textName:{
+          firstname:'',
+          lastname:'',
+        }
+      }
+
+    }
+
+  }
+</script>
+<style>
+.headerfnamelname{
+  text-align: center;
+}
+.showName{
+ margin-top: 30px;
+    max-width: 25%;
+    margin-left: 40%;
+    background-color: black;
+    color: wheat;
+
+}
+.inSide{
+    max-width: 40%;
+    margin-left: 30%;
+    margin-top: 30px;
+}
+.inputText{
+   background-color: black;
+    color: wheat;
+}
+</style>
+
